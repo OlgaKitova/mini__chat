a46caecf21bcf6def57c72368dc41a36a5b6001a
Olga Kitova <olga.vizgalova@bk.ru>
2021-07-10 17:35:07 +0500

    feat: add socket

--- mini__chat/server.js
+++ mini__chat/server.js
@@ -1,17 +1,26 @@
 
 const express = require('express');
+const socket = require('socket.io');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
+const server = require('http').Server(app);
+const io = socket(server);
 
 // database
 const rooms = new Map();
 
 app.get('/', (req, res) => {
-  res.json('ответ')
+  res.json(rooms)
 });
 
-app.listen(PORT, (err) => {
+//enable socket 
+io.on('connection', () => {
+  console.log("User connected")
+})
+
+
+server.listen(PORT, (err) => {
   if(err) throw Error(err);
   console.log("Сервер запущен")
 });
